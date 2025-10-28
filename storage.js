db321bdaded190961e4228eca13aa9d521c7ebd8
xanderselvatico <154509442+xanderselvatico@users.noreply.github.com>
2025-10-28 17:14:14 +0100

    implementate le funzioni di load e save delle informazioni
    le informazioni vengono salvate su un local storage

--- storage.js
+++ storage.js
@@ -1,1 +1,14 @@
+// storage.js
+const STORAGE_KEY = 'kanban_issues';
+
+// Carica tutte le issue salvate
+function loadIssues() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  return data ? JSON.parse(data) : [];
+}
+
+// Salva le issue aggiornate
+function saveIssues(issues) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(issues));
+}
 
